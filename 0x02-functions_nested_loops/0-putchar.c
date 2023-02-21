508fa7d34d26685f06c24956aa522079fcee0d8e
Franklintay <Franklin.kwame.tay@gmail.com>
2023-02-21 06:34:20 -0800

    0. _puchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int file1;
+
+	file1 = '_';
+	putchar(file1);
+	file1 = 'p';
+	putchar(file1);
+	file1 = 'u';
+	putchar(file1);
+	file1 = 't';
+	putchar(file1);
+	file1 = 'c';
+	putchar(file1);
+	file1 = 'h';
+	putchar(file1);
+	file1 = 'a';
+	putchar(file1);
+	file1 = 'r';
+	putchar(file1);
+	putchar('\n');
+	return (0);
+}
