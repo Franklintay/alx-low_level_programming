78185feb10a2966e2f3e4da8a7d77578d9267fd8
Franklintay <Franklin.kwame.tay@gmail.com>
2023-02-21 13:11:30 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ * @x: one of the integers
+ * @y: the other integer
+ * Return: Result of the addition
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
