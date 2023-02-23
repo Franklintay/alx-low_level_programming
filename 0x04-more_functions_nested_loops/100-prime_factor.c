a8ecb392502439a3bf09c270b24f75ce490dc861
Franklintay <Franklin.kwame.tay@gmail.com>
2023-02-23 14:03:38 -0800

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print the largest prime factor of a number
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long a, l;
+	long n = 612852475143;
+
+	for (a = 1; a++;)
+	{
+		if (n % a == 0)
+		{
+			l = n / a;
+		}
+	}
+	printf("%ld\n", l);
+	return (0);
+}
