4fbc5d2aeabf819a1b8356d407348f75e808524d
Franklintay <Franklin.kwame.tay@gmail.com>
2023-03-02 10:25:57 -0800

    ./1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,15 +9,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x;
-	int y;
+	int x = 0;
+	int y = 0;
 
-	for (x = 0; dest[x] != '\0'; x++)
-		;
-	for (y = 0; y <= n - 1; y++)
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (y < n && src[y] != '\0')
 	{
 		dest[x] = src[y];
 		x++;
+		y++;
 	}
+	dest[x] = '\0';
 	return (dest);
 }
