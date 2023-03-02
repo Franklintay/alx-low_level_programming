39c816e5b8517f0387ac70ff600661efb823fef2
Franklintay <Franklin.kwame.tay@gmail.com>
2023-03-02 10:32:08 -0800

    ./2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,9 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0 ; x < n ; x++)
+	while (x < n && src[x] != '\0')
+	{
 		dest[x] = src[x];
+		x++;
+	}
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
 	return (dest);
 }
