366a5a18ee05c23c7c7e907dd81cc48fd508d15e
Franklintay <Franklin.kwame.tay@gmail.com>
2023-03-08 16:03:09 -0800

    3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  * factorial - returns the factorial of a given number.
  * @n: a given number
+ *
  * Return: n is lower than 0, the function should return -1
  */
+
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
-	else
-		return (n * factorial(n -1));
+	return (n * factorial(n - 1));
 }
