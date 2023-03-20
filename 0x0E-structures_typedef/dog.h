a2479440d731241d7dad033569ad25a93f3e9acd
Franklintay <Franklin.kwame.tay@gmail.com>
2023-03-20 09:46:45 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_1_H
+#define HEADER_1_H
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: name of type
+ * @age: age of type
+ * @owner: owner of type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
